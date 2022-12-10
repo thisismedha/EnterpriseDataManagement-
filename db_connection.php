79f97a5e56c3e33f0394d584da8e6a64316b3d75
thisismedha01~ <medhasingh@arizona.edu>
2022-12-09 19:09:19 -0700

    Commit Database

--- db_connection.php
+++ db_connection.php
@@ -1,4 +1,4 @@
 <?php
-$connection = oci_connect('mis531groupS2L', 'XS9719FJbz+(9?Y', '//navydb.artg.arizona.edu:1521/COMPDB')
+$connection = oci_connect('username', 'password', 'connectionstring')
   or die(oci_error());
 ?>
